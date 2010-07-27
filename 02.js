0e78b10c515f44765e0e81f8b85ea7b9317365a8
Benedict Eastaugh <benedict@eastaugh.net>
2010-07-27 15:48:44 +0100

    Call, not apply.

--- 02.js
+++ 02.js
@@ -39,7 +39,7 @@ function detectType(value) {
 // constructor as an array.
 
 function buildObject(constructor) {
-    var args = Array.prototype.slice.apply(arguments, 1);
+    var args = Array.prototype.slice.call(arguments, 1);
     
     return new constructor(args);
 }
